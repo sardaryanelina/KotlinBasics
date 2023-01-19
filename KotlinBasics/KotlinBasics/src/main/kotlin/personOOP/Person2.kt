45a4500610f183034f64301344b4377aa17ac0fb
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2023-01-18 23:35:34 -0500

    OOP

---
+++ KotlinBasics/KotlinBasics/src/main/kotlin/personOOP/Person2.kt
@@ -0,0 +1,7 @@
+package personOOP
+
+import java.time.LocalDate
+
+class Person2(var name: String, var age: Int) {
+    var birthyear = LocalDate.now().year - age
+}
---
+++ KotlinBasics/KotlinBasics/src/main/kotlin/personOOP/Person3.kt
@@ -0,0 +1,24 @@
+package personOOP
+
+import java.time.LocalDate
+
+class Person3(name: String, age: Int) {
+    var name: String
+    var age: Int
+    var birthYear:Int
+    var currentYear = LocalDate.now().year
+
+    init {
+        println()
+        println("Object(Person3) was created")
+        this.name = name
+        this.age = age
+        this.birthYear = currentYear - age
+    }
+
+    fun getYearOfBirth() = currentYear - age
+
+    fun greet() {
+        println("Hello $name!")
+    }
+}
